9832bf2f8d7bef6b6bf26ea10a4157d076aaa3ce
Eksaron <149102408+Eksaron@users.noreply.github.com>
2024-05-18 20:04:21 +0000

    Deploying to gh-pages from @ Eksaron/goit-js-hw-10@40102e98c3df3c1d7c9b57a43b366fe9e85d347e 🚀

--- assets/2-snackbar-de13cfa7.js
+++
@@ -1,2 +0,0 @@
-import{i as n}from"./vendor-77e16229.js";const i=document.querySelector(".form");i.addEventListener("submit",t=>{t.preventDefault();const o=parseInt(i.delay.value),s=i.state.value;r(o,s).then(e=>{n.success({title:"Success",message:e,position:"topRight"}),console.log(`😊 ${e}`)}).catch(e=>{n.error({title:"Error",message:e,position:"topRight"}),console.log(`😡 ${e}`)})});function r(t,o){return new Promise((s,e)=>{setTimeout(()=>{o==="fulfilled"?s(`Fulfilled promise in ${t}ms`):e(`Rejected promise in ${t}ms`)},t)})}
-//# sourceMappingURL=2-snackbar-de13cfa7.js.map
